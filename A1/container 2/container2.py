64c125841443d6f9ce002ccd5b8d2e16c902834c
Siddik Aiyub Patel <sd591280@dal.ca>
2025-01-21 17:18:07 -0400

    7th try..sigh

--- A1/container 2/container2.py
+++ A1/container 2/container2.py
@@ -31,5 +31,4 @@ def calculate_sum():
         return jsonify({"file": file_name, "error": "Input file not in CSV format."}), 500
     
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5100)
