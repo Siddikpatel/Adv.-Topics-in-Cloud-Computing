b8b56a924abecebbdf2903be97e3b26a362e7821
Siddik Aiyub Patel <sd591280@dal.ca>
2025-03-05 18:36:58 -0400

    final

--- A2/main.py
+++ A2/main.py
@@ -3,10 +3,30 @@ import pymysql
 
 app = Flask(__name__)
 
+def db_connection():
+    try:
+        connection = pymysql.connect(
+            host='products-instance-1.c4xrlsohiz8m.us-east-1.rds.amazonaws.com',
+            user='admin',
+            password='Admin_123456',
+        )
+
+        with connection.cursor() as cursor:
+            cursor.execute("CREATE DATABASE IF NOT EXISTS products")
+            connection.commit()
+            cursor.execute("USE products")
+        
+        return connection
+
+    except pymysql.MySQLError as e:
+        print(f"Error: {e}")
+
+
 @app.route('/store-products', methods=['POST'])
 def store_data():
-    data = request.get_json()
-    
+    data = request.get_json()  
+    products = data['products']  
+    connection = db_connection()
     try:
         with connection.cursor() as cursor:
             create_table_sql = """
@@ -19,7 +39,8 @@ def store_data():
             """
             cursor.execute(create_table_sql)
             sql = "INSERT INTO products_table (name, price, availability) VALUES (%s, %s, %s)"
-            cursor.execute(sql, (data['name'], data['price'], data['availability']))
+            for product in products:
+                cursor.execute(sql, (product['name'], product['price'], product['availability']))
         connection.commit()
     finally:
         connection.close()
@@ -29,6 +50,7 @@ def store_data():
 @app.route('/list-products', methods=['GET'])
 def retrieve_data():
     
+    connection = db_connection()
     try:
         with connection.cursor() as cursor:
             sql = "SELECT * FROM products_table"
@@ -45,13 +67,6 @@ def retrieve_data():
         connection.close()
     
     return jsonify({'products': products}), 200
-
-connection = pymysql.connect(
-    host='products-instance-1.c4xrlsohiz8m.us-east-1.rds.amazonaws.com',
-    user='admin',
-    password='Admin_123456',
-    database='products'
-)
 
 if __name__ == '__main__':
     app.run(debug=True)
